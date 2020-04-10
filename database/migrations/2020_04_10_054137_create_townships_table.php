8a82b6e5b4fec16d72f5dc112baecf3062dee364
nainglintun <nainglintun1231@gmial.com>
2020-04-10 12:19:49 +0630

    create township table

---
+++ database/migrations/2020_04_10_054137_create_townships_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTownshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('townships', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('township_name');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('townships');
+    }
+}
